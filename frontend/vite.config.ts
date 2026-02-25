8203a6f0c10775532942a63d7873743826baa731
Jeffrey Mildenberg <jeffrey.mildenberg@gmail.com>
2026-02-24 19:13:56 -0500

    Update Vite.Config for testing api.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -15,7 +15,6 @@ export default defineConfig({
         target: 'http://localhost:8000',
         changeOrigin: true,
         ws: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
   },
